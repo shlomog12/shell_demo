a4b2ca9f63ee3849f76c0fe2fba712878fa77ade
shlomog12 <root@user-VirtualBox.(none)>
2022-06-07 01:59:08 +0300

    "finish"

--- myshell.c
+++ myshell.c
@@ -9,110 +9,45 @@
 #include <signal.h> 
 
 
-// void foo(int fd, char **argvs[]){
-//     printf("***********************\n");
-//     printf("arv1[0] = %s\n",argvs[0][0]);
-//     printf("arv2[0] = %s\n",argvs[1][0]);
-//     printf("arv3[0] = %s\n",argvs[2][0]);
-//     printf("***********************\n");
-//     int fildes[2];
-//     pipe (fildes);
-//     if (fork() == 0) { 
-//         /* first component of command line */ 
-//         close(STDOUT_FILENO); 
-//         dup(fildes[0]); 
-//         close(fildes[0]); 
-//         close(fildes[1]);
-//         close(fildes[2]); 
-//             /* stdout now goes to pipe */ 
-//             /* child process does command */ 
-//         execvp(argvs[0][0], argvs[0]);
-//     } 
-//         /* 2nd command component of command line */ 
-//     if (fork() == 0){
-//         close(STDIN_FILENO);
-//         dup(fildes[1]);
-//         close(fildes[1]); 
-//         close(fildes[0]);
-//         close(fildes[2]); 
-//         execvp(argvs[1][0], argvs[1]);
-
-//     }
-//     close(STDIN_FILENO);
-//     dup(fildes[2]);
-//     close(fildes[2]); 
-//     close(fildes[1]);
-//     close(fildes[0]); 
-//         /* standard input now comes from pipe */ 
-//     execvp(argvs[2][0], argvs[2]);
-// }
-
-
-
-static void pipeline(char ***cmd){
-	int fd[2];
-	pid_t pid;
-	int fdd = 0;				/* Backup */
-
-	while (*cmd != NULL) {
-		pipe(fd);				/* Sharing bidiflow */
-		if ((pid = fork()) == -1) {
-			perror("fork");
-			exit(1);
-		}
-		else if (pid == 0) {
-			dup2(fdd, 0);
-			if (*(cmd + 1) != NULL) {
-				dup2(fd[1], 1);
-			}
-			close(fd[0]);
-			execvp((*cmd)[0], *cmd);
-			exit(1);
-		}
-		else {
-			wait(NULL); 		/* Collect childs */
-			close(fd[1]);
-			fdd = fd[0];
-			cmd++;
-		}
-	}
-}
-
-
-// void foo4(){
-
-
-// }
-
-int runPipe(char ***cmd, int len, int amper){
+int runPipe(char ***argvs, int len, int amend, int redirect, char* outfile){
   	int fildes[2];
-	int fdd = 0;
+	int fd = 0;
     int status = -1;
     for (size_t i = 0; i < len; i++){
         pipe(fildes);
 		if (fork() == 0) {
-			dup2(fdd, 0);
+			dup2(fd, 0);
             if (i == len-1){
+                if (redirect == 1) {
+                    fd = creat(outfile, 0660); 
+                    close (STDOUT_FILENO) ; 
+                    dup(fd); 
+                    close(fd); 
+                }else if (redirect == 2){
+                    fd = creat(outfile, 0660); 
+                    close (STDERR_FILENO) ; 
+                    dup(fd); 
+                    close(fd); 
+                }
                 close(fildes[0]);
-			    status = execvp(cmd[i][0], cmd[i]);
+			    status = execvp(argvs[i][0], argvs[i]);
             }else{
                 dup2(fildes[1], 1);
                 close(fildes[0]);
-			    execvp(cmd[i][0], cmd[i]);
+			    execvp(argvs[i][0], argvs[i]);
             }
 			exit(1);
 		}else {
-            if (i == len-1 && amper == 0) wait(&status);
-            else wait(NULL); 
+            if (amend == 0){
+                if (i == len-1) wait(&status);
+                else  wait(NULL);
+            }
 			close(fildes[1]);
-			fdd = fildes[0];
+			fd = fildes[0];
 		}
     }
     return status;
 }
-
-
-
 
 void signal_handler(int signal){
     printf("\nYou typed Control-C!\n");
@@ -132,99 +67,110 @@ void insertArgv(char** argvs[], char** argv, int len, int pos){
     copyStr(argv, argvs[pos], len);
 }
 
+int initArgs(char ***argvs, int *lens){
+    char command[1024];
+    char *argv[10];
+
+    fgets(command, 1024, stdin);
+    command[strlen(command) - 1] = '\0';
+    int piping = 0;
+    int len = 0;
+    char *token = strtok (command," ");
+    while (token != NULL){
+        argv[len] = token;
+        token = strtok (NULL, " ");
+        len++;
+        if (token && !strcmp(token, "|")) {
+            insertArgv(argvs, argv, len, piping);
+            lens[piping] = len;
+            piping++;
+            len = 0;
+            token = strtok (NULL, " ");
+        }
+    }
+    argv[len] = NULL;
+    lens[piping] = len;
+    insertArgv(argvs,argv,len,piping);
+    argvs[piping+1] = NULL;
+    return piping;
+}
+
+void printArgvs(char ***argvs, int *lens, int piping){
+    for (size_t i = 0; i <= piping; i++){
+        int cur_len = lens[i];
+        printf("************* i = %ld len = %d  ****************\n",i,cur_len);
+        for (size_t j = 0; j < cur_len; j++){
+            printf("j =%ld com = %s\n",j,argvs[i][j]);
+        }
+    }
+}
+
+void saveEnv(char **current_commends){
+    int len = strlen(current_commends[0])+strlen(current_commends[1])+strlen(current_commends[2]);
+    char* str =(char*) malloc(len*sizeof(char));
+    strcat(str,current_commends[0]);
+    strcat(str,current_commends[1]);
+    strcat(str,current_commends[2]);
+    putenv(str);
+}
+
+void printEnv(char *key){
+    char val[30];
+    if (!getenv(key)) val[0] = '\0';
+    else snprintf(val, 10, "%s", getenv(key));
+    printf("%s",val);
+    if (val[strlen(val)-1] != '\n') printf("\n");
+}
 
 int main() {
     signal(SIGINT,  signal_handler);
-
-    char command[1024];
-    char *token;
     char *outfile;
-    char *argv[10], *temp[10];
+    char *temp[10];
     char **argvs[20];
-    int len, fd, amper, redirect, retid, status, temp_len, piping;
+    int temp_len = -1;
+    int len, amper, redirect, status, piping;
     int lens[20];
     int pos = 0;
 
     char* myprompt = "hello";
-    int x = 0;
-
-
+    // initArgs(argvs);
     while (1){
-        x++;
         printf("%s: ",myprompt);
-        fgets(command, 1024, stdin);
-        command[strlen(command) - 1] = '\0';
-
-        piping = 0;
-        /* parse command line */
-      
-        
-        len = 0;
-        token = strtok (command," ");
-        while (token != NULL){
-            argv[len] = token;
-            token = strtok (NULL, " ");
-            // printf("\nerror 70\n");
-            len++;
-            if (token && !strcmp(token, "|")) {
-                insertArgv(argvs, argv, len, piping);
-                lens[piping] = len;
-                piping++;
-                len = 0;
-                token = strtok (NULL, " ");
-            }
-        }
-        argv[len] = NULL;
-        lens[piping] = len;
-        insertArgv(argvs,argv,len,piping);
-        argvs[piping+1] = NULL;
-
-        // copyStr(argv,argvs[0],lens[0]);
-
-        // for (size_t i = 0; i <= piping; i++){
-        //     int cur_len = lens[i];
-        //     printf("************* i = %ld len = %d  ****************\n",i,cur_len);
-        //     for (size_t j = 0; j < cur_len; j++){
-        //         printf("j =%ld com = %s\n",j,argvs[i][j]);
-        //     }
-        // }
-
-
+        piping = initArgs(argvs,lens);
+        // printArgvs(argvs, lens, piping);
         char** current_commends = argvs[0];
         len = lens[0];
-        // printf("cur[0] = %s\n",current_commends[0]);
-        
 
         /* Is command empty */
         if (current_commends[0] == NULL) continue;
-        if (! strcmp(current_commends[0], "!!")){
+
+    // *****************start commants:*******************************
+        if (!strcmp(current_commends[0], "!!")){
+            if (temp_len == -1) continue;
             len = temp_len;
             copyStr(temp, current_commends, len);
         }else{
             copyStr(current_commends,temp,len);
             temp_len = len;
         }
-        if (!strcmp(current_commends[0], "quit")){
-            // return 0;
+        if (!strcmp(current_commends[0], "quit") || !strcmp(current_commends[0], "quit\n")){
             exit(0);
         }
-    // *****************start commants:*******************************
-
         if ((len > 1)&&  !strcmp(current_commends[0], "cd")){
             chdir(current_commends[1]);
         }
-
         if ((len > 1 ) && (! strcmp(current_commends[0], "echo")) && (! strcmp(current_commends[1], "$?"))){
             sprintf(current_commends[1],"%d",status);
         }
+        if ((len > 1 ) && (! strcmp(current_commends[0], "echo")) && current_commends[1][0]== '$'){
+            printEnv(current_commends[1]+1); continue;
+        }
+
+
         if ((len > 2) && (! strcmp(current_commends[0], "prompt")) && (! strcmp(current_commends[1], "="))){
             myprompt = current_commends[2];
             continue;
         }
-
-
-        
-
         /* Does command line end with & */ 
         if (! strcmp(current_commends[len - 1], "&")) {
             amper = 1;
@@ -237,39 +183,26 @@ int main() {
             current_commends[len - 2] = NULL;
             outfile = current_commends[len - 1];
             }
-        else if ((len > 2) && ! strcmp(current_commends[len - 2], "2>")){
+        else if ((len > 2) && !strcmp(current_commends[len - 2], "2>")){
             redirect = 2;
             current_commends[len - 2] = NULL;
             outfile = current_commends[len - 1];
-        }else
-            redirect = 0; 
+        }else redirect = 0; 
 
-        /* for commands not part of the shell command language */ 
-
-        int fildes[2];
-        // foo(redirect, piping, fd, outfile, fildes, argvs);
-
-
-        if (fork() == 0) { 
-            /* redirection of IO ? */
-            if (redirect == 1) {
-                fd = creat(outfile, 0660); 
-                close (STDOUT_FILENO) ; 
-                dup(fd); 
-                close(fd); 
-                /* stdout is now redirected */
-            }else if (redirect == 2){
-                fd = creat(outfile, 0660); 
-                close (STDERR_FILENO) ; 
-                dup(fd); 
-                close(fd); 
-            }
-            status = runPipe(argvs, piping+1, amper);
-            printf("\nstt77 = %d\n",status);
+        if (len > 1 && !strcmp(current_commends[0], "read")){
+            char val_of_read[1024];
+            fgets(val_of_read, 1024, stdin);
+            char* read_input[3] = {current_commends[1],"=",val_of_read};
+            saveEnv(read_input);
         }
+        if (len > 2 && current_commends[0][0] == '$'){
+            current_commends[0] = current_commends[0]+1;
+            saveEnv(current_commends);
+        }
+      
         
-        /* parent continues here */
-        // if (amper == 0) retid = wait(&status);
+        status = runPipe(argvs, piping+1,amper, redirect, outfile);
+
     }
 }
 
--- test.c
+++ test.c
@@ -45,21 +45,54 @@ static void pipeline(char ***cmd){
  * Compute multi-pipeline based
  * on a command list.
  */
-int
-main(int argc, char *argv[])
-{
+
+
+void setEnv(char *key, char* val){
+	if(!getenv(key)){
+        fprintf(stderr, "The environment variable %s was not found.\n", key);
+        exit(1);
+    }
+	if(snprintf(val, 10, "%s", getenv(key)) >= 10){
+        fprintf(stderr, "BUFSIZE of %d was too small. Aborting\n",10);
+        exit(1);
+    }
+}
+
+
+
+int main(int argc, char *argv[]){
+	putenv("person=moshe");
+	// char aa[10];
+	// char *envvar = "person";
+	// //  if(!getenv(envvar)){
+    // //     fprintf(stderr, "The environment variable %s was not found.\n", envvar);
+    // //     exit(1);
+    // // }
+	// if(!getenv(envvar)){
+    //     fprintf(stderr, "The environment variable %s was not found.\n", envvar);
+    //     exit(1);
+    // }
+	// if(snprintf(aa, 10, "%s", getenv(envvar)) >= 10){
+    //     fprintf(stderr, "BUFSIZE of %d was too small. Aborting\n",10);
+    //     exit(1);
+    // }
+	char val[20];
+	setEnv("person",val);
+    printf("person: %s\n",val) ;
+	// snprintf(aa, 10, "%s", getenv("person"));
+	// printf("aa =%s\n",aa);
 	// char *ls[] = {"ls", "-al", NULL};
 	// char *rev[] = {"rev", NULL};
 	// char *nl[] = {"nl", NULL};
 	// char *cat[] = {"cat", "-e", NULL};
-    char *cat[] = {"cat", "file1.txt", NULL};
-	char *sort[] = {"sort", NULL};
-	char *uniq[] = {"uniq", NULL};
+    // char *cat[] = {"cat", "file1.txt", NULL};
+	// char *sort[] = {"sort", NULL};
+	// char *uniq[] = {"uniq", NULL};
 
 
-	// char *cat[] = {"cat", "-e", NULL};
-	// char **cmd[] = {ls, rev, nl, cat, NULL};
-    char **cmd[] = {cat, sort, uniq, NULL};
-	pipeline(cmd);
+	// // char *cat[] = {"cat", "-e", NULL};
+	// // char **cmd[] = {ls, rev, nl, cat, NULL};
+    // char **cmd[] = {cat, sort, uniq, NULL};
+	// pipeline(cmd);
 	return (0);
 }
